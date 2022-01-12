5ae851791a58f3c4cc3d72670c6d886458ee0884
Ibrahim Bali <balii@pm.me>
2022-01-13 00:30:59 +0300

    feat: add sorter selection

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
+use clap::{ArgEnum, Parser};
 mod sample;
 mod sort;
 
+/// Compare sort algorithms
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: usize,
+
+    /// Sort using the algorithm chosen
+    #[clap(arg_enum, default_value_t)]
+    // #[clap(short, long, default_value_t = Sorter::QuickSort)]
+    sorter: Sorter,
+}
+
+#[derive(ArgEnum, Debug, Clone)]
+enum Sorter {
+    Selection,
+    Merge,
+    QuickSort,
+}
+
+impl Default for Sorter {
+    fn default() -> Self {
+        Self::QuickSort
+    }
+}
+
 fn main() -> Result<(), csv::Error> {
-    sample::write(2_usize.pow(1))?;
+    let args = Args::parse();
+    sample::write(2_usize.pow(14))?;
     let mut vec = sample::read()?;
 
     let now = std::time::Instant::now();
-    sort::with_selection(&mut vec);
-    // sort::with_merge(&mut vec);
-    // sort::with_quicksort(&mut vec);
+    match args.sorter {
+        Sorter::Selection => sort::with_selection(&mut vec),
+        Sorter::Merge => sort::with_merge(&mut vec),
+        Sorter::QuickSort => sort::with_quicksort(&mut vec),
+    };
     let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+    println!("Elapsed:\n{:.2?}", elapsed);
     sample::write_to(vec, "sorted.csv")
 }
